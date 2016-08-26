45d96dcbbc6ca01a94070eaa39c4270f609f01ad
Lock90 <lstaib@ksu.edu>
2016-08-26 14:50:01 -0500

    Update lightbikes.js

--- lightbikes.js
+++ lightbikes.js
@@ -1,4 +1,14 @@
 var canvas = document.getElementById('screen');
 var ctx = canvas.getContext('2d');
+var x = 0;
+var y = 0;
 
+
+function loop(){
+  
 ctx.fillRect(50,50,50,50);
+x++;
+setTimeout(loop,200)
+  
+}
+loop();
